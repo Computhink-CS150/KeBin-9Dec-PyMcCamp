9715dc0d1078b71f77822d78fe1aad4dd97189a9
Computhink-CS150 <cs150@computhink.com.sg>
2025-12-11 06:33:47 +0000

    Dec 11, 2025, 6:33 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -11,7 +11,7 @@ print("SIX SEVEN")
 # Task 2:
 title = input ("what is your title")
 name = input ("what is your name")
-command = ("what is your command")
+command = input ("what is your command")
 
 ########################################################################
 # Task 3:
